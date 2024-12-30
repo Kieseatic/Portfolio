d7329420513be3e44c56ce81da898c5e417ea785
Harsh <harshdugar1234@gmail.com>
2024-12-30 18:46:44 -0500

    Resolved nested repository issue

---
+++ Portfolio/components/Social.jsx
@@ -0,0 +1,33 @@
+import { FaLinkedin, FaGithub, FaDiscord, FaEnvelope } from "react-icons/fa";
+import { motion } from "framer-motion";
+
+const socials = [
+  { icon: <FaLinkedin />, path: "https://www.linkedin.com/in/solarluiso/" },
+  { icon: <FaGithub />, path: "https://github.com/solarluiso" },
+  { icon: <FaEnvelope />, path: "mailto:harshdugar1234@gmail.com" },
+];
+
+const Social = () => (
+  <div className="flex gap-4">
+    {/* Social Links */}
+    {socials.map((item, index) => (
+      <motion.a
+        key={index}
+        href={item.path}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="w-12 h-12 border border-accent rounded-full flex items-center justify-center text-accent text-xl hover:bg-accent hover:text-primary transition-all duration-500"
+        initial={{ opacity: 1 }}
+        animate={{ opacity: 1 }}
+        transition={{
+          delay: 0.1 * index,
+          duration: 0.5,
+        }}
+      >
+        {item.icon}
+      </motion.a>
+    ))}
+  </div>
+);
+
+export default Social;
